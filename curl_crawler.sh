6bf7ad14e028abdedda7f20b847f706dbce27a4a
Timur Aykut YILDIRIM <timur.aykut@gmail.com>
2014-12-03 15:38:15 +0200

    v2.0
    Controlling site’s status (200)
    Displaing counter for user

--- curl_crawler.sh
+++ curl_crawler.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 file="url-list.txt"
+COUNT=0
 
 while read line
-do 
-  outfile=$(echo $line | awk 'BEGIN { FS = "/" } ; {print $NF}')
-  curl -o "$outfile.html" "$line"
+do
+	STATUS=$(curl -s -o /dev/null -w "%{http_code}\n" "$line")
+	if [ $STATUS == 200 ] ; then
+		#echo "$line URL up, returned $STATUS"
+		COUNT=$((COUNT+1))
+		echo "$COUNT"
+		outfile=$(echo $line | awk 'BEGIN { FS = "/" } ; {print $NF}')
+		curl -o "$outfile.html" "$line" &> /dev/null
+	else
+	       echo "$line is not up, returned $STATUS"
+	fi
 done < "$file"
